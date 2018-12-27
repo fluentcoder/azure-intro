259f0e92318322d0993df338eeaf5bb6cd8f31ea
dim97 <dmitrijs.mamontovs@accenture.com>
2018-12-27 16:27:05 +0200

    Added key vault functionality: -get secrets -create secrets -create keys -get keys -delete keys -encrypt data by using keys -decrypt data by using keys

--- WebApp/WebApplication/Controllers/KeyVaultController.cs
+++ WebApp/WebApplication/Controllers/KeyVaultController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace WebApplication.Controllers
 {
@@ -14,7 +17,14 @@ namespace WebApplication.Controllers
     [ApiController]
     public class KeyVaultController : ControllerBase
     {
+        private string _keyVaultUri;
+
         public string Message { get; set; }
+
+        public KeyVaultController(IConfiguration iConfig)
+        {
+            _keyVaultUri = iConfig.GetSection("AppSettings").GetSection("KeyVaultUri").Value;
+        }
 
         // GET: api/KeyVault/secret/name
         [HttpGet("secret/{nameOfSecret}")]
@@ -23,15 +33,14 @@ namespace WebApplication.Controllers
             Message = ".";
             try
             {
-                /* The below 4 lines of code shows you how to use AppAuthentication library to fetch secrets from your Key Vault*/
                 AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
                 KeyVaultClient keyVaultClient = new KeyVaultClient(
                     new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
                 var secret = await keyVaultClient
-                    .GetSecretAsync("https://mykeyvaultdm.vault.azure.net/", nameOfSecret)
+                    .GetSecretAsync(_keyVaultUri, nameOfSecret)
                     .ConfigureAwait(false);
                 Message = secret.Value;
-
+                
                 return Ok("Message: " + Message);
             }
 
@@ -42,9 +51,186 @@ namespace WebApplication.Controllers
             {
                 Message = keyVaultException.Message;
 
-                return Ok("Exception: "+Message);
+                return BadRequest("Exception: "+Message);
             }
         }
+        // POST: api/KeyVault/secret/name
+        [HttpPost("secret/{nameOfSecret}")]
+        public async Task<IActionResult> CreateSecret([FromRoute] string nameOfSecret,[FromBody] string content)
+        {
+            Message = ".";
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret = await keyVaultClient.SetSecretWithHttpMessagesAsync(_keyVaultUri, nameOfSecret,content);
+                Message = secret.Body.Id;
 
+                return Ok("New secret Id: " + Message + Environment.NewLine + "Name of secret: " + nameOfSecret);
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                Message = keyVaultException.Message;
+
+                return BadRequest("Exception: " + Message);
+            }
+        }
+        // POST: api/KeyVault/key
+        [HttpPost("key")]
+        public async Task<IActionResult> CreateKey([FromBody] string[] parameters)
+        {
+            string nameOfKey = parameters[0];
+            string keyType = parameters[1];
+
+            Message = ".";
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var key = await keyVaultClient.CreateKeyWithHttpMessagesAsync(_keyVaultUri, nameOfKey, keyType);
+
+                var result = key.Body.Key;
+
+                return Ok("Key created !" + Environment.NewLine + 
+                          "Key identifier: "+result.Kid + Environment.NewLine + 
+                          "Key type: "+result.Kty
+                          );
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (Exception exception)
+            {
+                Message = exception.Message;
+
+                return BadRequest("Exception: " + Message);
+            }
+        }
+        // GET: api/KeyVault/key/name
+        [HttpGet("key/{name}")]
+        public async Task<IActionResult> GetKey([FromRoute] string name)
+        {
+            Message = ".";
+            string version;
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var lastVersion = keyVaultClient.GetKeyVersionsWithHttpMessagesAsync(_keyVaultUri, name).Result.Body.ToList().FirstOrDefault().Identifier.Version;
+                var key = await keyVaultClient.GetKeyWithHttpMessagesAsync(_keyVaultUri, name, lastVersion);
+                var result = key.Body.Key;
+
+                return Ok("Key received !" + Environment.NewLine +
+                          "Key identifier: " + result.Kid + Environment.NewLine +
+                          "Key type: " + result.Kty + Environment.NewLine +
+                          "Key last version: "+ lastVersion+ Environment.NewLine +
+                          "Request Id: " + key.RequestId 
+                );
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (Exception exception)
+            {
+                Message = exception.Message;
+
+                return BadRequest("Exception: " + exception);
+            }
+        }
+        // DELETE: api/KeyVault/key/name
+        [HttpDelete("key/{name}")]
+        public async Task<IActionResult> DeleteKey([FromRoute] string name)
+        {
+            Message = ".";
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                await keyVaultClient.DeleteKeyWithHttpMessagesAsync(_keyVaultUri, name);
+
+                return Ok("Key removed");
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (Exception exception)
+            {
+                Message = exception.Message;
+
+                return BadRequest("Exception: " + exception);
+            }
+        }
+        // POST: api/KeyVault/key/name
+        [HttpPost("key/{name}")]
+        public async Task<IActionResult> EncryptData([FromRoute] string name,[FromBody]string input)
+        {
+            Message = ".";
+
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                byte[] bytes = Encoding.ASCII.GetBytes(input);
+
+                var lastVersion = keyVaultClient.GetKeyVersionsWithHttpMessagesAsync(_keyVaultUri, name).Result.Body.ToList().FirstOrDefault().Identifier.Version;
+                var output = await keyVaultClient.EncryptWithHttpMessagesAsync(_keyVaultUri, name, lastVersion, "RSA-OAEP-256",bytes);
+
+                var output1 = await keyVaultClient.DecryptWithHttpMessagesAsync(_keyVaultUri, name, lastVersion, "RSA-OAEP-256", output.Body.Result);
+                return Ok(output.Body.Result);
+ 
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (Exception exception)
+            {
+                Message = exception.Message;
+
+                return BadRequest("Exception: " + exception);
+            }
+        }
+        // GET: api/KeyVault/key/value/name
+        [HttpGet("key/value/{name}")]
+        public async Task<IActionResult> DecryptData([FromRoute] string name, [FromBody]byte[] data)
+        {
+            Message = ".";
+
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var lastVersion = keyVaultClient.GetKeyVersionsWithHttpMessagesAsync(_keyVaultUri, name).Result.Body.ToList().FirstOrDefault().Identifier.Version;
+                var output = await keyVaultClient.DecryptWithHttpMessagesAsync(_keyVaultUri,name,lastVersion, "RSA-OAEP-256", data);
+                return Ok(Encoding.UTF8.GetString(output.Body.Result));
+            }
+
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (Exception exception)
+            {
+                Message = exception.Message;
+
+                return BadRequest("Exception: " + exception);
+            }
+        }
     }
 }
